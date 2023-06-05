8bd692cad4c81ea8400cf88109def0ee218e45c7
원종석 <@J_S_WON>
2023-06-05 22:33:31 +0900

    azimuth

---
+++ azimuth.m
@@ -0,0 +1,14 @@
+function az = azimuth(ENU)
+
+[m, n] = size(ENU);
+
+az = [];
+
+for num = 1:n
+    R_E = ENU(num,1);
+    R_N = ENU(num,2);
+    R_U = ENU(num,3);
+
+    az(num, 1) = acosd(R_N / sqrt(R_E^2 + R_N^2));
+end
+end
